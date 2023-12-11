551c0359f8e9b82f23da08e31dd5e6e60bd4f822
Abhijit Paul <bsse1201@iit.du.ac.bd>
2023-12-11 20:52:21 +0600

    Fixed the code so that it stores stuffs in a directory

--- script.py
+++ script.py
@@ -1,7 +1,19 @@
 import os
 from fontforge import *
 
-font = open(os.sys.argv[1])
+font_file = os.sys.argv[1]
+font = open(font_file)
+
+# Create a folder based on the font name
+font_name = os.path.splitext(os.path.basename(font_file))[0]
+output_folder = font_name + "_images"
+
+# Create the output folder if it doesn't exist
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+# Export each glyph as an image
 for glyph in font:
     if font[glyph].isWorthOutputting():
-        font[glyph].export(font[glyph].glyphname + ".png")
+        output_path = os.path.join(output_folder, font[glyph].glyphname + ".png")
+        font[glyph].export(output_path)
